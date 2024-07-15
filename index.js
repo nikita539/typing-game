4cd1c37b048ea5dad45eafa434c948051d3f639d
Nikita <evteev_n@inbox.ru>
2024-07-15 14:42:11 +0300

    created time cointer

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+// DOM - elements
 const startButton = document.getElementById('start_buton')
 const input = document.getElementById('input')
 const textField = document.getElementById('text_field')
 
-
+// Global variables
 const text = [
     "Hello my name is Nikita !",
     "I live in Russia",
 ]
-const splitedText = text[0].split(' ')
-
-// numbers of orders
 let cuurrentSentenceOrder = 0;
+const splitedText = text[cuurrentSentenceOrder].split(' ')
 let cuurrentWordOrder = 0;
+let startTime;
 
 
 startButton.addEventListener('click', function() {
@@ -25,7 +25,10 @@ startButton.addEventListener('click', function() {
         textField.appendChild(el)
     })
 
-    // disable start buton
+    // set start time
+    startTime = Date.now()
+
+    // disable start button
     startButton.disabled = true
     
 })
@@ -34,12 +37,21 @@ startButton.addEventListener('click', function() {
 input.addEventListener('input', function(event) {
 
     if (cuurrentWordOrder == splitedText.length - 1) {
-        alert(`game is end. Your score is ${cuurrentWordOrder} and ${splitedText.length - 1}`)
+        textField.childNodes[cuurrentWordOrder].className = 'highlite'
+        textField.childNodes[cuurrentWordOrder - 1].className = ''
+        event.target.value = ''
+        console.log(`your score is ${(Date.now() - startTime) / 1000}`)
+        return
     }
 
     if (event.target.value.trim() === splitedText[cuurrentWordOrder]) {
 
         textField.childNodes[cuurrentWordOrder].className = 'highlite'
+
+        if (cuurrentWordOrder > 0) {
+            textField.childNodes[cuurrentWordOrder - 1].className = ''
+        }
+
         cuurrentWordOrder++
         event.target.value = ''
 
